ea70c0519d9868f63c631e5df57a7c6285650707
johnfrojas98 <johnfrojas98@gmail.com>
2020-07-07 20:35:36 -0500

    Ajustando login

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -16,7 +16,11 @@ const Home = ({ myList, trends, originals }) => {
                 <Categories tittle="Mi lista">
                     <Carousel>
                         {myList.map(item =>
-                            <CarouselItem key={item.id} {...item} />
+                            <CarouselItem 
+                            key={item.id} 
+                            {...item} 
+                            isList
+                            />
                         )}
                     </Carousel>
                 </Categories>
